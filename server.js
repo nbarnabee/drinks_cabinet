a31d8966194232f71d76d7a24fef6ed3a4f3f6cc
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-19 17:04:41 +0200

    Enable basic backend functions

--- server.js
+++ server.js
@@ -2,32 +2,32 @@ const express = require("express");
 const app = express();
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+//const MongoStore = require("connect-mongo")(session);
 const flash = require("express-flash");
 const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 
 require("dotenv").config({ path: "./config/.env" });
-require("./config/passport")(passport);
+// require("./config/passport")(passport);
 
-connectDB();
+//connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded({extended: true});
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-app.use(
+/*app.use(
   session({
-   secret: "monitor dog",
-   resave: false,
-   saveUninitialized: false,
-   store 
+    secret: "monitor dog",
+    resave: false,
+    saveUninitialized: false,
+    store,
   })
-)
-app.use(passport.initialize());
-app.use(passport.session());
+);*/
+// app.use(passport.initialize());
+// app.use(passport.session());
 app.use(flash());
 
 app.use("/", mainRoutes);
