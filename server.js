2dda26bcdf4e2ec98f399927bf89c019e25e51cb
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-19 16:49:50 +0200

    Initial commit of backend files

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+
+require("dotenv").config({ path: "./config/.env" });
+require("./config/passport")(passport);
+
+connectDB();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true});
+app.use(express.json());
+app.use(logger("dev"));
+app.use(
+  session({
+   secret: "monitor dog",
+   resave: false,
+   saveUninitialized: false,
+   store 
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use("/", mainRoutes);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}`);
+});
