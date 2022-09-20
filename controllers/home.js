f2cdf03c42afcdedeed3895b1d2dce1c090060dc
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-09-20 11:17:17 +0200

    Expand document structure

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,4 @@
-const MongoClient = require("mongodb").MongoClient;
+const mongoConnect = require("../config/database");
 
 module.exports = {
   getIndex: (req, res) => {
@@ -11,25 +11,31 @@ module.exports = {
 
   addRecipe: async (req, res) => {
     try {
-      const client = new MongoClient(process.env.DB_STRING);
+      const client = mongoConnect.getConnection();
       const db = client.db("drinks_cabinet");
+      // The ingredients are uploaded as two arrays, one containing the ingredients and the other containing the corresponding measures.  Here I associate them with each other and insert them into an object.  (But I have to do something differently if there's only one ingredient.. which I doubt there would be but you never know)
       const ingredientObj = {};
-      req.body.ingredient.forEach((a, i) => {
-        ingredientObj[a] = req.body.measure[i];
-      });
+      if (typeof req.body.ingredient === "string") {
+        ingredientObj[req.body.ingredient] = req.body.measure;
+      } else
+        req.body.ingredient.forEach((a, i) => {
+          ingredientObj[a] = req.body.measure[i];
+        });
       const newRecipe = {
-        recipe: req.body.recipe,
+        name: req.body.recipe,
         category: req.body.category,
         "sub-category": req.body.subcategory,
+        favorited: 0, // For future use
+        rating: [Number(req.body.rating)], // rating will be an array of numbers and later I'll take their average
         flavor: req.body.flavor,
-        alcohol: req.body.alcohol,
+        alcohol: Number(req.body.alcohol),
         glass: req.body.glass,
         ingredients: ingredientObj,
         instructions: req.body.instructions,
       };
       console.log(newRecipe);
-      // const result = await db.collection("recipes").insertOne(newRecipe);
-      // console.log(`A document was inserted with the _id: ${result.insertedId}`);
+      const result = await db.collection("recipes").insertOne(newRecipe);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
       res.render("addRecipe.ejs");
     } catch (err) {
       console.log(err);
